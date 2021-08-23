f13cd2131e2bda619eba820278d6a4f72e17cc8f
Cesar Camacho <icesardcm99@gmail.com>
2021-08-23 17:44:51 -0500

    Estiliza las 6 habilidades en el grid

--- pages/index.js
+++ pages/index.js
@@ -89,7 +89,7 @@ export default function Home() {
 
         {/*  ---------------------------------- Skils ----------------------------------  */}
 
-        <div id="Skills" className="flex flex-col mx-auto mt-36 ">
+        <div id="Skills" className="flex flex-col mx-auto mt-36 mb-36 ">
           <h2 className="text-4xl font-medium">Skills</h2>
           <div className="w-3/5">
             {" "}
@@ -99,7 +99,7 @@ export default function Home() {
             </p>{" "}
           </div>
 
-          <div className="grid items-center grid-cols-3 gap-4 mt-10 font-semibold text-center ">
+          <div className="grid items-center w-4/5 grid-cols-3 mt-10 font-semibold text-center gap-x-10 gap-y-5 ">
             <div className="flex flex-col p-6 overflow-hidden bg-gray-200 rounded-3xl justify-items-center ">
               <img
                 src="images/js.png"
@@ -107,35 +107,35 @@ export default function Home() {
               ></img>
               <p>Javascript</p>
             </div>
-            <div className="flex flex-col p-6 overflow-hidden bg-gray-200 rounded justify-items-center ">
+            <div className="flex flex-col p-6 overflow-hidden bg-gray-200 rounded-3xl justify-items-center ">
               <img
                 src="images/react.png"
                 className="object-contain w-full h-12"
               ></img>
               <p>React</p>
             </div>
-            <div className="flex flex-col p-6 overflow-hidden bg-gray-200 rounded justify-items-center ">
+            <div className="flex flex-col p-6 overflow-hidden bg-gray-200 rounded-3xl justify-items-center ">
               <img
                 src="images/next.png"
                 className="object-contain w-full h-12"
               ></img>
               <p>Next.js</p>
             </div>
-            <div className="flex flex-col p-6 overflow-hidden bg-gray-200 rounded justify-items-center ">
+            <div className="flex flex-col p-6 overflow-hidden bg-gray-200 rounded-3xl justify-items-center ">
               <img
                 src="images/node.png"
                 className="object-contain w-full h-12"
               ></img>
               <p>Node</p>
             </div>
-            <div className="flex flex-col p-6 overflow-hidden bg-gray-200 rounded justify-items-center ">
+            <div className="flex flex-col p-6 overflow-hidden bg-gray-200 rounded-3xl justify-items-center ">
               <img
                 src="images/figma.png"
                 className="object-contain w-full h-12"
               ></img>
               <p>Figma</p>
             </div>
-            <div className="flex flex-col p-6 overflow-hidden bg-gray-200 rounded justify-items-center ">
+            <div className="flex flex-col p-6 overflow-hidden bg-gray-200 rounded-3xl justify-items-center ">
               <img
                 src="images/webflow.png"
                 className="object-contain w-full h-12"
