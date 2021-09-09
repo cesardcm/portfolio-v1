51332a5847a5f6160f15dc3e5f805434b54943df
Cesar Camacho <icesardcm99@gmail.com>
2021-09-08 20:18:15 -0500

    Fixed dark mode toggle button bug, that shows darker colors than expected on each mode of the page, added inside shadow instead

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,29 +4,31 @@ export default function Navbar() {
   const [colorTheme, setTheme] = useDarkMode();
 
   return (
-    <header className="sticky top-0 z-50 py-4 mx-auto font-medium backdrop-blur-lg">
+    <header className="sticky top-0 z-50 pt-2 pb-1 mx-auto font-medium backdrop-blur-lg">
       <nav className="flex flex-row items-center justify-between max-w-4xl mx-auto mb-2 ">
         <div className="items-center text-2xl ">
           <h2 className="">César Camacho</h2>
         </div>
         <div className="flex items-center text-xl">
-          <a className="ml-8 hover:underline" href="#about">
-            About
-          </a>
-          <a className="ml-8 hover:underline" href="#contact">
-            Contact
-          </a>
-          <div className="pl-8 text-4xl">
+          <div className="pr-4">
+            <a className="ml-8 hover:underline" href="#about">
+              About
+            </a>
+            <a className="ml-8 hover:underline" href="#contact">
+              Contact
+            </a>
+          </div>
+          <div className="w-4 h-full pl-8 text-4xl">
             {colorTheme === "light" ? (
               <button
-                className="px-3 py-2 bg-gray-600 bg-opacity-50 rounded-lg backdrop-blur-lg "
+                className="px-3 py-2 rounded-lg shadow-inner "
                 onClick={() => setTheme("light")}
               >
                 💡
               </button>
             ) : (
               <button
-                className="px-1 py-2 bg-gray-200 bg-opacity-50 rounded-lg backdrop-blur-lg "
+                className="px-1 py-2 rounded-lg shadow-inner "
                 onClick={() => setTheme("dark")}
               >
                 🌙
