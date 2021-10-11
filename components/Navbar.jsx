92aff7f25f6a75833b6e5689dd9dc4cdc5c35756
thejamdev <thejamdeveloper@gmail.com>
2021-10-11 11:01:18 -0500

    Added dark apple style color to glassphormism in navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,6 @@
 export default function Navbar() {
   return (
-    <header className="sticky top-0 z-50 px-4 py-4 mx-auto font-medium lg:pt-6 backdrop-blur-sm">
+    <header className="sticky top-0 z-50 px-4 py-4 mx-auto font-medium bg-black bg-opacity-25 lg:pt-6 backdrop-blur-sm ">
       <nav className="flex flex-row items-center justify-between max-w-sm mx-auto mb-2 sm:max-w-lg md:max-w-xl lg:max-w-4xl ">
         <div className="items-center text-xl font-bold text-white lg:text-2xl ">
           <a href="#">
